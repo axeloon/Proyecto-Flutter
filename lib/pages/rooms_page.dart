9dde341d3201a06605b7a94119971ef30780990b
Diego <dcastilloa@utem.cl>
2023-12-01 18:37:54 -0300

    Corrección y manejo de errores y casos de búsqueda

--- lib/pages/rooms_page.dart
+++ lib/pages/rooms_page.dart
@@ -9,70 +9,103 @@ class RoomsPage extends StatefulWidget {
 
 class _RoomsPageState extends State<RoomsPage> {
   late Future<List<Room>> _fetchRoomsFuture;
+  int _minCapacity = 0;
 
   @override
   void initState() {
     super.initState();
-    _fetchRoomsFuture = _fetchRooms();
+    _fetchRoomsFuture = RoomService.fetchRooms();
   }
 
-  Future<List<Room>> _fetchRooms() async {
-    try {
-      final List<Room> rooms = await RoomService.fetchRooms();
-      return rooms;
-    } catch (error) {
-      throw Exception('Error al obtener las salas: $error');
-    }
+  List<Room> _filterRoomsByCapacity(List<Room> rooms) {
+    return _minCapacity != -1
+        ? rooms.where((room) => room.capacity >= _minCapacity).toList()
+        : [];
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Salas Disponibles'),
-      ),
-      body: Container(
-        color: Color.fromRGBO(186, 240, 240, 1), // Color de fondo deseado
-        child: FutureBuilder<List<Room>>(
-          future: _fetchRoomsFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            } else {
-              List<Room>? rooms = snapshot.data;
-              if (rooms != null && rooms.isNotEmpty) {
-                return ListView.builder(
-                  itemCount: rooms.length,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      margin: const EdgeInsets.all(8.0),
-                      child: ListTile(
-                        title: Text('Nombre: ${rooms[index].name}'),
-                        subtitle: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Ubicación: ${rooms[index].location}'),
-                            Text('Código: ${rooms[index].code}'),
-                            Text(
-                                'Capacidad: ${rooms[index].capacity.toString()}'),
-                          ],
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: const Text('Salas Disponibles'),
+    ),
+    body: Container(
+      color: const Color.fromRGBO(186, 240, 240, 1),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Capacidad mínima',
+                suffixIcon: Icon(Icons.search),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _minCapacity = value.isEmpty
+                      ? 0
+                      : value.contains(RegExp(r'^[0-9]+$'))
+                      ? int.parse(value)
+                      : -1;
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder<List<Room>>(
+              future: _fetchRoomsFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text(
+                      'Error al cargar las salas. Por favor, inténtelo nuevamente.',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  );
+                } else {
+                  List<Room> allRooms = snapshot.data ?? [];
+                  List<Room> filteredRooms = _filterRoomsByCapacity(allRooms);
+
+                  return filteredRooms.isNotEmpty
+                      ? ListView.builder(
+                    itemCount: filteredRooms.length,
+                    itemBuilder: (context, index) {
+                      final room = filteredRooms[index];
+                      return Card(
+                        margin: const EdgeInsets.all(8.0),
+                        child: ListTile(
+                          title: Text(
+                            room.name,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16.0,
+                            ),
+                          ),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Ubicación: ${room.location}'),
+                              Text('Código: ${room.code}'),
+                              Text('Capacidad: ${room.capacity.toString()}'),
+                            ],
+                          ),
+                          onTap: () {
+                            // Acciones al tocar una sala (Opcional por el momento)
+                          },
                         ),
-                        onTap: () {
-                          // Acciones al tocar una sala
-                        },
-                      ),
-                    );
-                  },
-                );
-              } else {
-                return const Center(child: Text('No hay salas disponibles.'));
-              }
-            }
-          },
-        ),
+                      );
+                    },
+                  )
+                      : const Center(child: Text('No hay salas disponibles.'));
+                }
+              },
+            ),
+          ),
+        ],
       ),
-    );
-  }
+    ),
+  );
 }
+
